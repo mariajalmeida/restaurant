350c6fb798c337e5edadf3e74cfd7b873e4f429a
mariajalmeida <mari9d91@stud.kea.dk>
2020-08-29 14:48:36 +0200

    updated images

--- script/script.js
+++ script/script.js
@@ -1,54 +1,52 @@
-// redo the filters into vanilla JS
+const productlistLink = "https://kea-alt-del.dk/t5/api/productlist";
+const categoriesLink = "https://kea-alt-del.dk/t5/api/categories";
+const productsLinkid = "https://kea-alt-del.dk/t5/api/product?id=";
+const template = document.querySelector("#myTemplate").content;
 
-// add 'allergens' icon-link under each card
+let image_path = "http://kea-alt-del.dk/t5/site/imgs/";
+let mediumImg = image_path + "medium/";
 
-// mansory to make the filters work
 
 
 // fetch data
 
-fetch("https://kea-alt-del.dk/t5/api/productlist?category=starter")
+fetch("https://kea-alt-del.dk/t5/api/productlist")
     .then(function (response) {
         console.log(response)
         return response.json();
     })
     .then(function (data) {
-
         dataReceived(data);
     })
 
 function dataReceived(products) {
-    products.forEach(showProduct)
+    products.forEach(showProduct);
 }
 
 function showProduct(myProduct) {
-    console.log(myProduct)
-    // to make a clone + the template needs this to be visible
+    console.log(myProduct);
 
+    // to make a clone + the template needs this to be visible
     const elTemplate = document.querySelector("#myTemplate").content;
 
     // el clone
-
     const myClone = elTemplate.cloneNode(true);
 
     // to change the content
-    //  myClone.querySelector(".discount").textContent = myProduct.id;
     myClone.querySelector("h3").textContent = myProduct.name;
     myClone.querySelector(".price").textContent = myProduct.price + " " + "-,";
     myClone.querySelector(".description").textContent = myProduct.shortdescription;
     myClone.querySelector(".discount").textContent = myProduct.discount + " " + "-,";
-    //    myClone.querySelector(".soldOut").textContent = "product out of stock";
 
-    // noStock();
+    myClone.querySelector("img").src = mediumImg + myProduct.image + "-md.jpg";
+
 
     // myClone.querySelector("img").src = myProduct.image;
 
     // to change the new 'parent' element
-
     const parentEl = document.querySelector("section#starter");
 
     // append the clone to the DOM
-
     parentEl.appendChild(myClone);
 
 
@@ -56,10 +54,15 @@ function showProduct(myProduct) {
 
     if (myProduct.soldout) {
         console.log("sold out indeed");
-        document.querySelector(".course").style.opacity = 50;
+        document.querySelector(".course").style.opacity = 0;
 
     } else {
         console.log("not sold out");
         document.querySelector(".course").style.opacity = 100;
+    }
+    if (myProduct.vegetarian) {
+        console.log("vegeterian indeed");
+    } else {
+        console.log("not vegetarian appropriate");
     }
 }
